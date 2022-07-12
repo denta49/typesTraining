5c961d2f0a424dbfdb2a525c39f782cda1e9229f
przemekWojenka <pdentaw@gmail.com>
2022-07-12 15:18:14 +0200

    add structure

---
+++ my-app/src/components/Todos.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Todos = () => {
+
+    return (
+        <ul>
+            <li>Learn React</li>
+            <li>Lear TypeScript</li>
+
+        </ul>
+
+    );
+};
+
+export default Todos;
