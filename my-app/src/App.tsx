734075443e4038f386ad1757b2c38d3476447ae2
przemekWojenka <pdentaw@gmail.com>
2022-07-13 10:10:10 +0200

    class add

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [new Todo("Learn react"), new Todo("Learn ts")];
   return (
     <div>
       <Todos items={["Learn react", "Learn Ts"]} />
