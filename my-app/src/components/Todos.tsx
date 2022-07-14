ef111e492188010e3cf27e5f270c1960894c5c19
przemekWojenka <pdentaw@gmail.com>
2022-07-14 14:54:29 +0200

    add remove feature

--- my-app/src/components/Todos.tsx
+++ my-app/src/components/Todos.tsx
@@ -3,11 +3,17 @@ import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
 import classes from "./Todos.module.css";
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
+  props
+) => {
   return (
     <ul className={classes.list}>
       {props.items.map((item) => (
-        <TodoItem text={item.text} key={item.id} />
+        <TodoItem
+          text={item.text}
+          key={item.id}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
